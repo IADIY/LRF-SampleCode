b499443dd09885cfd8a2659e90c3422f773b772b
Daniel Ferro - 方力揚 <36524661+ferrowism@users.noreply.github.com>
2024-04-02 11:04:50 +0800

    Style Update + README

--- LRF2K1LS/LRF2K1LS-samplecode.py
+++ LRF2K1LS/LRF2K1LS-samplecode.py
@@ -3,43 +3,68 @@
 # $ sudo pip install pyserial
 # =============================================================================
 
-import serial 
+import serial.tools.list_ports
+import sys
 import threading
 
 #declare
 global_flag=True
-#serial port
-ser = serial.Serial()
-#ser.port = '/dev/ttyUSB0' #(Linux)Note usb port should reflect which port the module is plugged in to - use ls /dev/ttyUSB* command to identify port
-ser.port = 'COM4'          #(Windows)Note usb port should reflect which port the module is plugged in to COM port, usw device manager to identify port
-ser.baudrate = 19200
-ser.bytesize = 8
-ser.parity = 'N'
-ser.topbits = 1
-#ser.timeout = 3000
-ser.open()
+
+#List available COMport
+ports = serial.tools.list_ports.comports()
+portlist=[]
+index=0
+for port, desc, hwid in ports:
+    portlist.append(port)
+    print(str(index)+") "+desc+"|"+port)
+    index+=1
+if portlist == []:
+    print("No serial ports detected")
+    sys.exit()
+
+#Select COMport
+portname=input("Please input the number of the desired port: \n")
+try:
+    ser = serial.Serial(
+        port=portlist[int(portname)],\
+        baudrate=19200,\
+        parity=serial.PARITY_NONE,\
+        stopbits=serial.STOPBITS_ONE,\
+        bytesize=serial.EIGHTBITS,\
+        timeout=3)
+except Exception as e:
+    print(str(e))
+    sys.exit()
  
 def receive():
     while(global_flag):
         #Read all data from the buffer
         data=ser.read_all()
         if(len(data)>=4):
-            print(data)
-            #When reading the measured distance returned by the module.
+            print('Module response: ' +str(data))
+            #Depending on the response, perform different tasks
+            #When reading a range finder result
             if data[1]==0x01:
+                #Data 2nd byte's 7th bit states if distance is invalid
                 if((data[2]>>7)&1)==1:
                     print("Distance is invalid")
                     continue
+                
+                #Data 2nd byte's 6th bit states if angle is invalid
                 if((data[2]>>6)&1)==1:
                     print("Angle is invalid")
                     continue
+
+                #Data 2nd byte's 5th bit states measurement's data resolution
                 if((data[2]>>5)&1)==0:
                     dataResolution=0.5
                 else:
                     dataResolution=0.1
+
                 #Calculate distance
                 distance=(data[3]*256+data[4])*dataResolution
                 print("Distance: "+str(round(distance,2))+ "mm")
+
                 #Calculate angle
                 if(data[5]<180):
                     angle=data[5]
@@ -47,21 +72,29 @@ def receive():
                     angle=data[5]-256
                 print("Angle: "+ str(round(angle,2))+" degree")
                
-            #When reading the status of module
+            #When reading the status of  the module
             elif data[1]==0x20:
                 info=''
+
+                #Data 2nd byte's 7th bit displays measurement state
                 if((data[2]>>7)&1)==0:
                     info+='Finish ranging, '
                 else:
                     info+='Laser rangefinder is busy, '
+
+                #Data 2nd byte's 6th bit states if an error occured
                 if((data[2]>>6)&1)==0:
                     info+='No Error\n'
                 else:
                     info+='In Error\n'
+
+                #Data 2nd byte's 1st bit displays tilt angle sensor state
                 if((data[2]>>1)&1)==0:
                     info+='Tilt angle sensor off, '
                 else:
                     info+='Tilt angle sensor ON, '
+
+                #Data 2nd byte's 0th bit states tilt angle sensor status
                 if((data[2]>>0)&1)==0:
                     info+='Tilt angle sensor nornal'
                 else:
@@ -75,35 +108,47 @@ def main(args=None):
     global global_flag
     t = threading.Thread(target = receive)
     t.start()
-    #Provide instructions for users
-    print("\nPress the following commands:")
-    print("   a: Single Measure (meter)")
-    print("   s: Continuous Measure (meter)")
-    print("   d: Stop Measurement")  
-    print("   f: Read Status")  
-    print("   q: Exit the program.")  
-    #console operationq
+ 
+    #console operation
     while global_flag:
-        #laser on
-        key=input("input:")
-        if key=='a':
-            ser.write(b'\x10\x83\x00\x7D')
-        #laser off 
+        #Provide instructions for the user 
+        key=input("\nPlease enter the operation command:\n -s single measurement (meter)\n\
+                   -c continuous measurement (meter)\n -x stop measurement\n\
+                   -r read status\n -q Exit\n")
+
+        #Single measurement (meter)
         if key=='s':
-            ser.write(b'\x10\x83\x40\x3D')
-        #laser measurment 
-        if key=='d':
             ser.reset_input_buffer()
-            ser.write(b'\x10\x84\x7C')
-        if key=='f':
+            cmd=bytearray(b'\x10\x83\x00\x7D')
+            ser.write(cmd)    
+
+        #Continuous measurement (meter)
+        elif key=='c':
             ser.reset_input_buffer()
-            ser.write(b'\x10\x80\x80')
-        #quit out of
-        if key=='q': 
-            global_flag=False   
-              
-    ser.close()  
-    print("End")
+            cmd=bytearray(b'\x10\x83\x40\x3D') 
+            ser.write(cmd)   
+
+        #Stop measurement
+        elif key=='x':
+            ser.reset_input_buffer()
+            cmd=bytearray(b'\x10\x84\x7C')
+            ser.write(cmd) 
+        
+        #Read status
+        elif key=='r':
+            ser.reset_input_buffer()
+            cmd=bytearray(b'\x10\x80\x80')
+            ser.write(cmd) 
+            
+        #Exit
+        elif key=='q':
+            global_flag = False
+            ser.close()
+            sys.exit()
+
+        #Incorrect Input
+        else:
+            print("Invalid option. Please enter one of the above options.")
  
 if __name__ == '__main__':
     main()
---
+++ LRF3K1LS/README.md
@@ -0,0 +1,23 @@
+# 3km Long-Range Laser Rangefinder Module - LRF3K1LS
+## Introduction
+The 3km Long-Range Laser Rangefinder Module (LRF3K1LS), produced by IADIY, is a classic model for outdoor long-range laser rangefinder. The measurement range up to 3000 meters, with low power consumption and stable performance that up to the industrial and military standard. The compact, eye safe and highly integrated laser rangefinder module is utilized in various applications from versatile systems to handheld devices.
+## Communication
+
+### UART Parameters
+- Baud Rate: 600~19200
+- Parity Bits: None
+- Stop Bits: 1
+- Byte Size: 8
+
+### Command List
+| Command | Command Code |
+| --- | --- |
+| Single Measurement (meter) | 10 83 00 7D |
+| Continuous Measurement (meter) | 10 83 40 3D |
+| Stop Measurement | 10 84 7C |
+| Read Status | 10 80 80 |
+
+## More Details
+To learn more about this product and to see a full data sheet, please visit: https://www.iadiy.com/laser-module/laser-sensor/laser-distance-sensor/3km-laser-rangefinder-module
+
+To learn more about IADIY and browse other great products please visit: https://www.iadiy.com/
